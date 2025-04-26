babce3d0bb36958a0719a3a71ff1b57211ea82df
Thameem <thameem@MacBookPro.lan>
2025-04-25 20:42:26 -0400

    Optimized product except self

--- array/product-of-array-except-self.cpp
+++ array/product-of-array-except-self.cpp
@@ -39,5 +39,39 @@ class Solution {
                 }
             }
             return answer;
+             // vector<int> answer(nums.size());
+            // int p = 1;
+            // int zcount = 0;
+            // for(int i = 0; i < nums.size(); i++ )
+            // {
+            //     if(nums[i] != 0)
+            //     {
+            //         p = p * (int)nums[i];
+            //     }
+            //     else
+            //     {
+            //         zcount++;
+            //     }
+    
+            // }
+            // if(zcount > 1)
+            //     return answer;
+            // for(int i = 0; i < nums.size(); i++ )
+            // {
+                
+            //     if( nums[i] == 0)
+            //     {
+            //         answer[i] = p;
+            //     }
+            //     else if( nums[i] != 0 and zcount == 1)
+            //     {
+            //         answer[i] = 0;
+            //     }
+            //     else
+            //     {
+            //         answer[i] = p/nums[i];
+            //     }
+            // }
+            // return answer;
         }
     };
