ff8cc28cb7f52f3d1db3530886ec9df996baa94f
Thameem <thameem@MacBookPro.lan>
2025-05-18 17:45:17 -0400

    Modified h index

--- array/h-index.cpp
+++ array/h-index.cpp
@@ -2,30 +2,52 @@
 using namespace std;
 class Solution {
     public:
-        int hIndex(vector<int>& citations) {
-            sort(citations.begin(), citations.end());
-            int l = citations.size();
-            int temp = citations[0];
-            for(int i = 1; i<l; i++)
+    int hIndex(vector<int>& citations) {
+        // sort(citations.begin(), citations.end());
+        // int l = citations.size();
+        // int temp = citations[0];
+        // for(int i = 1; i<l; i++)
+        // {
+        //     if(i == l-1 && temp == 0 && citations[i] > 0)
+        //     {
+        //         return 1;
+        //     }
+        //     int hl = l - i;
+        //     if(citations[i] <= hl)
+        //     {
+        //         temp = max(temp, citations[i]);
+        //     }
+        //     else
+        //     {
+        //         temp = max(temp, hl);
+        //     }
+        // }
+        // if(temp > l)
+        // {
+        //     temp = min(temp, l);
+        // }
+        // return temp;
+        vector<int> count(citations.size()+1);
+        for(int a: citations)
+        {
+            if(a < citations.size())
             {
-                if(i == l-1 && temp == 0 && citations[i] > 0)
-                {
-                    return 1;
-                }
-                int hl = l - i;
-                if(citations[i] <= hl)
-                {
-                    temp = max(temp, citations[i]);
-                }
-                else
-                {
-                    temp = max(temp, hl);
-                }
+                count[a]++;
             }
-            if(temp > l)
+            else
             {
-                temp = min(temp, l);
+                count[citations.size()]++;
             }
-            return temp;
         }
-    };
+        int papCount = 0;
+        for(int i = count.size() - 1; i > 0; i--)
+        {
+            papCount+=count[i];
+            if(papCount >= i)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
